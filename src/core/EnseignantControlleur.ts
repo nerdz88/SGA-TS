1e7c6573f6b18865b8d371d20fd99a3bb5eb05d9
lionel.bewa <lionel.bewa@gmail.com>
2021-05-24 22:58:52 -0400

    Récupérer de cours avec prof hardcodé

--- src/core/EnseignantControlleur.ts
+++ src/core/EnseignantControlleur.ts
@@ -87,17 +87,17 @@ export class EnseignantControlleur {
     }
 
     public async recupererCours(tokenEnseignant: string) {
-        
-        const reponse = await fetch(this.baseUrl+this.endPoint+"courses", {headers: { token: tokenEnseignant}})
+
+        const reponse = await fetch(this.baseUrl + this.endPoint + "courses", { headers: { token: tokenEnseignant } })
         const json = await reponse.json();
         return json;
-        
+
     }
 
-    public async recupererDetailCour(tokenEnseignant: string, id: string) {
+    public async recupererDetailCours(tokenEnseignant: string, id: string) {
 
-        const path = "course/"+id+"/students"
-        const reponse = await fetch(this.baseUrl+this.endPoint+path, {headers: { token: tokenEnseignant}})
+        const path = "course/" + id + "/students"
+        const reponse = await fetch(this.baseUrl + this.endPoint + path, { headers: { token: tokenEnseignant } })
         console.log(reponse);
         const json = await reponse.json()
         return json;
