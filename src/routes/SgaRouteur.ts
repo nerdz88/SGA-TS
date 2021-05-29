9610c1ef8e46e5a9076bd18c26dcbc78b5a13ccc
lionel.bewa <lionel.bewa@gmail.com>
2021-05-29 11:53:55 -0400

    Refractor route login
    Co-authored-by: nerdz88 <nerdz88@users.noreply.github.com>
    Co-authored-by: Tommy Bédard <Tommy-TI@users.noreply.github.com>

--- src/routes/SgaRouteur.ts
+++ src/routes/SgaRouteur.ts
@@ -20,13 +20,59 @@ export class SgaRouteur {
         this.init();
     }
 
-    public pageAccueil(req: Request, res: Response, next: NextFunction) {
+    /**
+     * Methode GET pour afficher la page d'accueil
+     */
+    public accueilPage(req: Request, res: Response, next: NextFunction) {
         //TODO - login - afficher le nom de l'utilisateur dans accueil.pug
         if (!req.session.loggedIn) {
-            res.sendStatus(401);
-            return;
+            res.redirect("/login");
         }
-        res.render("enseignant/accueil");
+        else {
+            res.render("enseignant/accueil");
+        }
+    }
+
+    /**
+     * Methode GET pour afficher la page de login
+     */
+    public loginPage(req: Request, res: Response, next: NextFunction) {
+        if (req.session.loggedIn) {
+            res.redirect("/");
+        }
+        else {
+            res.render('login', { title: 'Service Gestion des Apprentissages' });
+        }
+    }
+
+    /**
+     * Methode qui permet de login
+     */
+    public login(req: Request, res: Response, next: NextFunction) {
+        let email = req.body.email as string;
+        let password = req.body.password as string;
+
+        let reponse = this.controlleur.login(email, password);
+        console.log("post login")
+        reponse.then(response => {
+            if ("message" in response && response["message"] == "Success") {
+                let token = response["token"];
+                req.session.email = email;
+                req.session.loggedIn = true;
+                req.session.token = token;
+                // Cookies not working, i have to add the cookie-parser to the App.ts middleware method, for now let's
+                // just store the token in the session
+                //res.cookie("token", token);
+                (req as any).flash('Requete details des etudiants dans un cour');
+                res.status(200);
+                res.redirect("/");
+            } else {
+                req.session.loggedIn = false;
+                (req as any).flash('Unauthorized');
+                res.status(400);
+                res.redirect("/login");
+            }
+        });
     }
 
     public pageAjouterCours(req: Request, res: Response, next: NextFunction) {
@@ -55,7 +101,7 @@ export class SgaRouteur {
 
         self.controlleur.ajouterCours(tokenEnseignant, idCours)
             .then(() => res.redirect("/api/v1/sga/enseignant/cours/" + idCours + "/detail"))
-            .catch(function (error) {               
+            .catch(function (error) {
                 self._errorCode500(error, req, res);
             });
     }
@@ -87,37 +133,11 @@ export class SgaRouteur {
         //TODO remove le token hardcodé apres le login
         let tokenEnseignant = (req.headers.token ? req.headers.token : req.session.token) as string
 
-        let idCours = req.params.id;
-        res.render("enseignant/detail-cours", { cours: this.controlleur.recupererUnCoursSGA(tokenEnseignant, idCours) });
+        let sigleCours = req.params.sigle;
+        res.render("enseignant/detail-cours", { cours: this.controlleur.recupererUnCoursSGA(tokenEnseignant, sigleCours) });
     }
 
-    /**
-     * Methode qui permet de login
-     */
-    public login(req: Request, res: Response, next: NextFunction) {
-        let email = req.query.email as string;
-        let password = req.query.password as string;
-        let reponse = this.controlleur.login(email, password);
-        reponse.then(response => {
-            if ("message" in response && response["message"] == "Success") {
-                let token = response["token"];
-                req.session.email = email;
-                req.session.loggedIn = true;
-                req.session.token = token;
-                // Cookies not working, i have to add the cookie-parser to the App.ts middleware method, for now let's
-                // just store the token in the session
-                //res.cookie("token", token);
-                (req as any).flash('Requete details des etudiants dans un cour');
-                res.status(200);
-                res.redirect("/api/v1/sga/enseignant/accueil");
-            } else {
-                req.session.loggedIn = false;
-                (req as any).flash('Unauthorized');
-                res.status(400);
-                res.redirect("/");
-            }
-        });
-    }
+
 
     private _errorCode500(error: any, req, res: Response<any>) {
         var code = 500;
@@ -133,12 +153,27 @@ export class SgaRouteur {
      * endpoints.
      */
     init() {
-        this.router.get('/enseignant/accueil', this.pageAccueil.bind(this));
-        this.router.get('/enseignant/cours', this.recupererCours.bind(this));
-        this.router.get('/enseignant/cours/ajouter', this.pageAjouterCours.bind(this));
-        this.router.get('/enseignant/cours/ajouter/:id', this.ajouterCours.bind(this));
-        this.router.get('/enseignant/cours/:id/detail', this.recupererDetailCours.bind(this));
-        this.router.get('/login/', this.login.bind(this))
+
+        //Accueil
+        this.router.get('/', this.accueilPage.bind(this));
+
+        //Login
+        this.router.get('/login', this.loginPage.bind(this));
+        this.router.post('/login', this.login.bind(this));
+
+
+
+        //Cours
+        this.router.get('/enseignant/cours', this.recupererCours.bind(this)); // Détail d'un cours
+        this.router.get('/enseignant/cours/:sigle', this.recupererDetailCours.bind(this)); // Détail d'un cours
+        this.router.get('/enseignant/cours/ajouter', this.pageAjouterCours.bind(this)); //La page pour ajouter un cours 
+        this.router.post('/enseignant/cours/ajouter', this.ajouterCours.bind(this)); //Le post ajouter un cours 
+
+        // this.router.get('/enseignant/cours', this.recupererCours.bind(this));
+        // this.router.get('/enseignant/cours/ajouter', this.pageAjouterCours.bind(this));
+        // this.router.get('/enseignant/cours/ajouter/:id', this.ajouterCours.bind(this));
+        // this.router.get('/enseignant/cours/:id/detail', this.recupererDetailCours.bind(this));
+        // this.router.get('/login/', this.login.bind(this))
     }
 }
 
