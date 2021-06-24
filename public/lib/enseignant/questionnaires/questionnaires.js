6f7e35908c8c54ae380fb3ad3572907b36198f80
nerdz88 <60405217+nerdz88@users.noreply.github.com>
2021-06-24 14:58:07 -0400

    push

---
+++ public/lib/enseignant/questionnaires/questionnaires.js
@@ -0,0 +1,33 @@
+//Fichier Javascript pour les pages de question
+
+// Si vous modifiez ce fichier, exécutez "npm run build" pour que votre server utilise la nouvelle version. Sinon le navigateur conserve l'ancienne version en cache.
+window.addEventListener("load", function () {
+    $(".btn-delete-question").on("click", function () {
+        var nomQuestion = $(this).data("nom");
+        var idQuestion = $(this).data("idQuestion");
+        var idEspaceCours = $(this).data("idEspaceCours");
+        $.confirm({
+            title: 'Confirmation',
+            backgroundDismiss: true,
+            content: 'Voulez-vous vraiment supprimer cette question: ' + nomQuestion,
+            buttons: {
+                confirm: function () {
+                    $.ajax({
+                        type: "DELETE",
+                        url: "/api/v1/enseignant/question/supprimer/" + idEspaceCours  + "/" + idQuestion,
+                        success: function () {
+                            var endpoint = "/enseignant/question" + (idEspaceCours ? "/" + idEspaceCours : "");
+                            window.location.href = endpoint;
+                        },
+                        error: function () {
+                            console.log("Supprimer Question - KO");
+                            showErrorToast("Erreur", "La question n'a pas été supprimé");                         
+                        }
+                    });
+                }
+            }
+        });
+    });
+
+    console.log("question.js => Page Load");
+});
