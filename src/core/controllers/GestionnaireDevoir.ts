e24adc5ee32fd485798d695cc461dbb470b6e61d
elsaso <sobhi.el-safady.1@ens.etsmtl.ca>
2021-06-24 13:56:12 -0400

    Ajout du backend de Devoir avant test

--- src/core/controllers/GestionnaireDevoir.ts
+++ src/core/controllers/GestionnaireDevoir.ts
@@ -8,8 +8,38 @@ export class GestionnaireDevoir{
         this.universite = universite;
     }
 
-    public ajouterDevoir(idEspaceCours: ){
+    public ajouterDevoir(idEspaceCours: number, devoirJsonString: string){
+        let espaceCours = this.universite.recupererUnEspaceCours(idEspaceCours);
+        espaceCours.ajouterDevoir(devoirJsonString);
+    }
 
+    public modifierDevoir(idEspaceCours: number, IdDevoir: number, jsonString: string) {
+        let espaceCours = this.universite.recupererUnEspaceCours(idEspaceCours);
+        espaceCours.modifierDevoir(IdDevoir, jsonString);
+    }
+
+/*
+    public recupererTousDevoirs(idEnseignant: number): string {
+        let arrayEspaceCours = this.universite.recupererTousEspaceCours(idEnseignant)
+        return JSON.stringify(arrayEspaceCours.flatMap(ec => {
+            let devoirs = ec.recupererTousDevoirs();
+            return devoirs.length > 0 ? devoirs : []
+        }));
+    }*/
+
+    public recupererTousDevoirsEspaceCours(idEspaceCours: number): string {
+        let espaceCours = this.universite.recupererUnEspaceCours(idEspaceCours);
+        return JSON.stringify(espaceCours.recupererTousDevoirs());
+    }
+
+    public recupererUnDevoir(idEspaceCours: number, IdDevoir: number) {
+        let espaceCours = this.universite.recupererUnEspaceCours(idEspaceCours);
+        return JSON.stringify(espaceCours.recupererUnDevoir(IdDevoir));
+    }
+
+    public supprimerDevoir(idEspaceCours: number, IdDevoir: number): boolean {
+        let espaceCours = this.universite.recupererUnEspaceCours(idEspaceCours);
+        return espaceCours.suprimerDevoir(IdDevoir)
     }
 
 }
