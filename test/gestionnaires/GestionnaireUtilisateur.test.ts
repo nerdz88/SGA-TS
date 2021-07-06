c002c7c8533e8d6a10ccc6414e30cf723edae6bc
nerdz88 <60405217+nerdz88@users.noreply.github.com>
2021-07-06 12:44:45 -0400

    test gestionnaireQuestionnaire + gestionaireUtilisateur

---
+++ test/gestionnaires/GestionnaireUtilisateur.test.ts
@@ -0,0 +1,13 @@
+import 'jest-extended';
+import { GestionnaireUtilisateur } from "../../src/core/controllers/GestionnaireUtilisateur";
+
+describe("Test gestionnaire Utilisateur", ()=>{
+
+    it("Devrais lancer une erreur lors de l'echec de l'appel", async()=>{
+
+        let gu = new GestionnaireUtilisateur()
+        expect(gu.login("randomemail@gmail.com","wrongPassword")).toThrowError
+
+    })
+
+})
