e07a3cfe2f49477528a82b8d9fedb0bf7575b877
nerdz88 <60405217+nerdz88@users.noreply.github.com>
2021-07-07 11:07:39 -0400

    WebAppRouteur test

--- test/routeur/WebAppRouteur.test.ts
+++ test/routeur/WebAppRouteur.test.ts
@@ -11,25 +11,25 @@ let idQuestion = "1";
 let idDevoir = "1";
 
 let pathPages = [
-    `/`,
-    `/enseignant/cours`,
-    `/enseignant/cours/ajouter`,
-    `/enseignant/cours/detail/${idEspaceCours}`,
-    `/enseignant/question/`,
-    `/enseignant/question/${idEspaceCours}`,
-    `/enseignant/question/detail/${idEspaceCours}/${idQuestion}`,
-    `/enseignant/question/ajouter/${idEspaceCours}`,
-    `/enseignant/question/modifier/${idEspaceCours}/${idQuestion}`,
-    `/enseignant/devoir/${idEspaceCours}`,
-    `/enseignant/devoir/detail/${idEspaceCours}/${idDevoir}`,
-    `/enseignant/devoir/ajouter/${idEspaceCours}`,
-    `/enseignant/devoir/modifier/${idEspaceCours}/${idDevoir}`,
-    `/enseignant/questionnaire/`,
-    `/enseignant/questionnaire/${idEspaceCours}`,
-    `/enseignant/questionnaire/detail/${idEspaceCours}/${idQuestionnaire}`,
-    `/enseignant/questionnaire/ajouter/${idEspaceCours}`,
-    `/enseignant/questionnaire/modifier/${idEspaceCours}/${idQuestionnaire}`,
-    `/enseignant/questionnaire/question/${idEspaceCours}/${idQuestionnaire}`,
+    {path: "/", titre: "Bonjour"},
+    {path: `/enseignant/cours`, titre: "Vos Espaces Cours"},
+    {path: `/enseignant/cours/ajouter`, titre: "Ajouter un Espace Cours"},
+    {path: `/enseignant/cours/detail/${idEspaceCours}`, titre: "Détail Espace Cours"},
+    {path: `/enseignant/question/`, titre: "Vos Questions"},
+    {path: `/enseignant/question/${idEspaceCours}`, titre: "Questions pour"},
+    {path: `/enseignant/question/detail/${idEspaceCours}/${idQuestion}`, titre: "Détail Question"},
+    {path: `/enseignant/question/ajouter/${idEspaceCours}`, titre: "Ajouter une question"},
+    {path: `/enseignant/question/modifier/${idEspaceCours}/${idQuestion}`, titre: "Modifier une question"},
+    {path: `/enseignant/devoir/${idEspaceCours}`, titre: "Devoir pour"},
+    {path: `/enseignant/devoir/detail/${idEspaceCours}/${idDevoir}`, titre: "Détail Devoir"},
+    {path: `/enseignant/devoir/ajouter/${idEspaceCours}`, titre: "Ajouter un devoir"},
+    {path: `/enseignant/devoir/modifier/${idEspaceCours}/${idDevoir}`, titre: "Modifier un devoir"},
+    {path: `/enseignant/questionnaire/`, titre: "Vos Questionnaires"},
+    {path: `/enseignant/questionnaire/${idEspaceCours}`, titre: "Questionnaires pour"},
+    {path: `/enseignant/questionnaire/detail/${idEspaceCours}/${idQuestionnaire}`, titre: "Détail Questionnaire"},
+    {path: `/enseignant/questionnaire/ajouter/${idEspaceCours}`, titre: "Ajouter un questionnaire"},
+    {path: `/enseignant/questionnaire/modifier/${idEspaceCours}/${idQuestionnaire}`, titre: "Modifier un questionnaire"},
+    {path: `/enseignant/questionnaire/question/${idEspaceCours}/${idQuestionnaire}`, titre: "Gérer les questions du questionnaire"},
 ]
 
 var authenticatedSession;
@@ -45,11 +45,12 @@ beforeAll((done) => {
 });
 
 describe('Webapp routeur test PAGE', () => {
-    pathPages.forEach((path: string) => {
-        it("RécupérerPage==> " + path, async (done) => {
-            let response = await authenticatedSession.get(path);
+    pathPages.forEach((value) => {
+        it("RécupérerPage==> " + value.path, async (done) => {
+            let response = await authenticatedSession.get(value.path);
             expect(response.status).toBe(200);
             expect(response.type).toContain("text/html");
+            expect(response.text).toContain(value.titre);
             done();
         });
     });
